27671efc98c83b1e4957fd496fa386c09033cd63
rosaninha <rosana.ea@gmail.com>
2020-11-12 21:19:32 -0500

    contact us

---
+++ Contact us form/Webpage/WebForm1.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Webpage.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/8148383c3c.js" crossorigin="anonymous"></script>
+    <link href="StyleSheet1.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div class="container">
+                <div class="contact-parent">
+                    <div class="contact-child child1">
+                        <p>
+                            <i class="fas fa-map-marker-alt"></i> Address <br />
+                            <span> 2417 Dundas West street
+                                <br />
+                                Toronto, ON
+                            </span>
+                        </p>
+                        <p>
+                            <i class="fas fa-phone-alt"></i> Let's talk <br />
+                            <span>6476765691</span>
+                        </p>
+                        <p>
+                            <i class="far fa-envolope"></i> General Support <br />
+                            <span>2021rosana2021@gmail.com</span>
+                        </p>
+                    </div>
+
+                    <div class="contact-child child2">
+                        <div class="inside-contact">
+                            <h2>Contact Us</h2>
+                            <h3>
+                            <asp:Label ID="confirm" runat="server" Text="We'd love to hear from you, please drop us a line if you've any query."></asp:Label>
+                                </h3>
+                            <p>Name *</p>
+                            <asp:TextBox ID="txt_name" runat="server" Required="required"></asp:TextBox>
+
+                            <p>Email *</p>
+                            <asp:TextBox ID="txt_email" runat="server" Required="required"></asp:TextBox>
+
+                            <p>Phone *</p>
+                            <asp:TextBox ID="txt_phone" runat="server" Required="required"></asp:TextBox>
+
+                            <p>Subject *</p>
+                            <asp:TextBox ID="txt_subject" runat="server" Required="required"></asp:TextBox>
+
+                            <p>Message *</p>
+                            <asp:TextBox ID="txt_message" runat="server" Required="required" TextMode="MultiLine" Rows="4"></asp:TextBox>
+
+                            <asp:Button ID="btn_send" runat="server" Text="SEND" OnClick="btn_send_Click" />
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Contact us form/Webpage/WebForm1.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Drawing;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace Webpage
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_send_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var from = "2021rosana2021@gmail.com";
+                var to = "2021rosana2021@gmail.com";
+                const string Password = "Netrio84";
+                string mail_subject = txt_subject.Text.ToString();
+                string mail_message = "From: " + txt_name.Text + "\n";
+                mail_message += "Email: " + txt_email.Text + "\n";
+                mail_message += "Phone: " + txt_phone.Text + "\n";
+                mail_message += "Subject: " + txt_subject.Text + "\n";
+                mail_message += "Message: " + txt_message.Text + "\n";
+
+                var smtp = new SmtpClient();
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Credentials = new NetworkCredential(from, Password);
+                    smtp.Timeout = 20000;
+                }
+
+                smtp.Send(from, to, mail_subject, mail_message);
+
+                Confirm();
+                confirm.Text = "Thank you for your email!";
+
+                txt_name.Text = "";
+                txt_email.Text = "";
+                txt_phone.Text = "";
+                txt_subject.Text = "";
+                txt_message.Text = "";
+
+            }
+            catch (Exception ex)
+
+            {
+                confirm.Text = "Something went wrong! Please try again.\nError code:" + ex.Message;
+                confirm.ForeColor = Color.Red;
+
+            }
+        }
+        private void Confirm()
+        {
+            string ToEmail = txt_email.Text.Trim();
+            string UserName = txt_name.Text;
+            string subject2 = txt_subject.Text;
+
+            MailMessage mailMessage = new MailMessage("2021rosana2021@gmail.com", ToEmail);
+
+            StringBuilder sbEmailBody = new StringBuilder();
+            sbEmailBody.Append("Dear" + UserName);
+            sbEmailBody.Append("<br/><br/>");
+            sbEmailBody.Append("Thank you for your email");
+            sbEmailBody.Append("<br/><br/>");
+            sbEmailBody.Append("We received your email regarding " + subject2 + "<br/>");
+            sbEmailBody.Append("We will be back to you as soon as possible.");
+            sbEmailBody.Append("<br/><br/><br/>");
+            sbEmailBody.Append("Sincerely, ");
+            sbEmailBody.Append("Master Service");
+
+            mailMessage.IsBodyHtml = true;
+
+            mailMessage.Body = sbEmailBody.ToString();
+            mailMessage.Subject = "Re: Thank you for your email";
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.Credentials = new System.Net.NetworkCredential("2021rosana2021@gmail.com", "Netrio84");
+
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(mailMessage);
+
+
+
+        }
+    }
+}
