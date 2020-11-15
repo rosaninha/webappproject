e94554c2bff74b26bc30deef77aeb12758db76a1
rosaninha <rosana.ea@gmail.com>
2020-11-15 00:17:36 -0500

    final exam

---
+++ CollegeManagementSystem/CollegeManagementSystem/Mainpage.aspx
@@ -0,0 +1,118 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Mainpage.aspx.cs" Inherits="CollegeManagementSystem.Mainpage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        
+    <style type="text/css">
+        .auto-style2 {
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <br />   
+           <center> <asp:Label ID="Label1" runat="server" Text="College Management System" Font-Bold="True" Font-Size="XX-Large" ForeColor="Green"></asp:Label></center>
+              <br />
+            <table class="table table-hover table-responsive">
+            
+
+            <tr>
+                <td>
+
+        <p id="lblContact">
+            StudentId<asp:TextBox ID="txtContact" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            </p>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+
+        <p id="lblname">
+            FirstName<asp:TextBox ID="txtName" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            </p>
+                </td>
+            </tr>
+
+                <tr>
+                    <td>
+
+        <p id="lblname2">
+            LastName<asp:TextBox ID="txtName2" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            </p>
+                </td>
+            </tr>
+
+                <tr>
+                    <td>
+
+        <p id="lblbirth">
+            BirthDate<asp:TextBox ID="txtbirth" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            </p>
+                </td>
+            </tr>
+
+                <tr>
+                    <td>
+
+        <p id="lblEmail">
+            EmailAddr<asp:TextBox ID="txtEmail" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            </p>
+                 </td>
+                    </tr>
+                 <tr>
+                    <td>
+
+        <p id="lblcountry">
+            Country<asp:TextBox ID="txtcountry" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            </p>
+                     </td>
+                  </tr>
+                <tr>
+                   
+            <p>
+                 <td class="auto-style2">
+                <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="Add" Cssclass="btn btn-default btn-success"/>
+                </td>
+
+                <td class="auto-style2">
+                <asp:Button ID="btnUpdate" runat="server" Enabled="false" OnClick="btnEdid_Click" Text="Update" CssClass=" btn-danger btn-default" />
+                </td>
+            </p>  
+                  </tr>
+
+               
+
+          </table>
+            <br />
+
+             <asp:GridView ID="gridBook" runat="server" class="table table-hover table-responsive">
+                 <Columns>
+                     <asp:HyperLinkField DataNavigateUrlFields="Id" DataNavigateUrlFormatString="Mainpage.aspx?id={0}&amp;action=1" HeaderText="Delete" Text="Delete">
+                     <FooterStyle ForeColor="Red" />
+                     </asp:HyperLinkField>
+                     <asp:HyperLinkField DataNavigateUrlFields="Id" DataNavigateUrlFormatString="Mainpage.aspx?id={0}&amp;action=2" HeaderText="Update" Text="Update">
+                     <FooterStyle ForeColor="Green" />
+                     </asp:HyperLinkField>
+                 </Columns>
+            </asp:GridView>
+            <br />
+            <td>
+                <asp:Label ID="lblUserDetails" runat="server" Text=""></asp:Label>
+            </td>
+            <br />
+            <td class="auto-style2">
+                <asp:Button ID="btnLogout" runat="server" OnClick="btnLogout_Click" Text="Logout" Cssclass="btn btn-default btn-danger"/>
+            </td>
+   </div>     
+    </form>
+</body>
+</html>
---
+++ CollegeManagementSystem/CollegeManagementSystem/Mainpage.aspx.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CollegeManagementSystem
+{
+    public partial class Mainpage : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        SqlCommand cmd;
+        SqlDataReader dr;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["id"] != null && Request.QueryString["action"] != null)
+                {
+                    string id = Request.QueryString["id"];
+                    string action = Request.QueryString["action"];
+                    string FirstName = null;
+                    string LastName = null;
+                    string BirthDate = null;
+                    string EmailAddr = null;
+                    string Country = null;
+                    con.Open();
+                    cmd = new SqlCommand("select * from Students where Id='" + id + "'", con);
+                    dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        FirstName = dr["FirstName"].ToString();
+                        LastName = dr["LastName"].ToString();
+                        BirthDate = dr["BirthDate"].ToString();
+                        EmailAddr = dr["EmailAddr"].ToString();
+                        Country = dr["Country"].ToString();
+                    }
+                    con.Close();
+                    if (action == "1")
+                    {
+                        con.Open();
+                        cmd = new SqlCommand("delete from Students where id = '" + id + "'", con);
+                        int checkD = cmd.ExecuteNonQuery();
+                        if (checkD == 1)
+                        {
+                            ClientScript.RegisterStartupScript(this.GetType(), "deleted", "<script>alert('Student deleted!');" +
+                                "location='Mainpage.aspx';</script>");
+                        }
+                        else
+                        {
+                            ClientScript.RegisterStartupScript(this.GetType(), "failed", "<script>alert(Failed!!!);</script>");
+                        }
+                        con.Close();
+                    }
+                    else if (action == "2")
+                    {
+                        btnAdd.Enabled = false;
+                        btnUpdate.Enabled = true;
+                        Session["id"] = id;
+                        txtName.Text = FirstName;
+                        txtName2.Text = LastName;
+                        txtbirth.Text = BirthDate;
+                        txtEmail.Text = EmailAddr;
+                        txtcountry.Text = Country;
+                    }
+
+                }
+                con.Open();
+
+                cmd = new SqlCommand("select * from Students", con);
+                dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    gridBook.DataSource = dr;
+                    gridBook.DataBind();
+                }
+            }
+            con.Close();
+            if (Session["username"] == null)
+                Response.Redirect("Login.aspx");
+
+            lblUserDetails.Text = "Username : " + Session["username"];
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand("insert into Students values(@FirstName,@LastName,@BirthDate,@EmailAddr,@Country)", con);
+            cmd.Parameters.Add("@FirstName", txtName.Text);
+            cmd.Parameters.Add("@LastName", txtName2.Text);
+            cmd.Parameters.Add("@BirthDate", txtbirth.Text);
+            cmd.Parameters.Add("@EmailAddr", txtEmail.Text);
+            cmd.Parameters.Add("@Country", txtcountry.Text);
+            int count = cmd.ExecuteNonQuery();
+            if (count == 1)
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "added", "<script>alert('Student Added successfully!' );" +
+                    "location='Mainpage.aspx';</script>");
+                txtName.Text = "";
+                txtName2.Text = "";
+                txtbirth.Text = "";
+                txtEmail.Text = "";
+                txtcountry.Text = "";
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "failed", "<script>alert('Failed! Please try again...!!!' );</script>");
+
+            }
+            con.Close();
+        }
+
+        protected void btnEdid_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand("update Student set FirstName=@FirstName, LastName=@LastName,BirthDate=@BirthDate, EmailAddr=@EmailAddr, Country=@Country where id = '" + Session["id"] + "'", con);
+            cmd.Parameters.Add("@FirstName", txtName.Text);
+            cmd.Parameters.Add("@LastName", txtName2.Text);
+            cmd.Parameters.Add("@BirthDate", txtbirth.Text);
+            cmd.Parameters.Add("@EmailAddr", txtEmail.Text);
+            cmd.Parameters.Add("@Country", txtcountry.Text);
+            int checkD = cmd.ExecuteNonQuery();
+            if (checkD == 1)
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "updated", "<script>alert('Contact updated!');location='Mainpage.aspx';</script>");
+                txtName.Text = "";
+                txtName2.Text = "";
+                txtbirth.Text = "";
+                txtEmail.Text = "";
+                txtcountry.Text = "";
+                btnAdd.Enabled = true;
+                btnUpdate.Enabled = false;
+                Session.Abandon();
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "updatefailed", "<script>alert('Failed!!!');</script>");
+            }
+            con.Close();
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
